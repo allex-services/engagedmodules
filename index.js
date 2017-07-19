e1fd63e3eed885c2527f4ec3771bad22ca306f56
Veljko Popovic <v.s.popovic@gmail.com>
2017-07-19 16:42:06 +0200

    updating dependencies

---
+++ index.js
@@ -0,0 +1,13 @@
+function createServicePack(execlib){
+  'use strict';
+  return {
+    service: {
+      dependencies: ['allex_dataservice']
+    },
+    sinkmap: {
+      dependencies: ['allex_dataservice']
+    },
+  };
+}
+
+module.exports = createServicePack;
